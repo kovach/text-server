57b3b7d4e5e46cf98283b26ca006be56b4e9b9f0
Scott Kovach <dskovach@gmail.com>
2016-02-11 20:08:34 -0800

    switch to lazy bytestring

--- demo-src/Main.hs
+++ demo-src/Main.hs
@@ -1,7 +1,8 @@
 module Main where
-import qualified Data.Text as T
+import qualified Data.Text.Lazy as T (pack)
+import qualified Data.Text.Lazy.Encoding as T (encodeUtf8)
 import Network.TextServer
 
-counter _ i = (T.pack $ "msgs: " ++ (show i), i+1)
+counter _ i = return (Just $ T.encodeUtf8 $ T.pack $ "msgs: " ++ (show i), i+1)
 
 main = runServer 0 counter
--- src/Network/TextServer.hs
+++ src/Network/TextServer.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Network.TextServer where
+module Network.TextServer (runServer, Handler) where
 
 -- imports lol
 import Data.Monoid (mappend)
@@ -12,12 +12,15 @@ import Control.Concurrent (MVar, newMVar, modifyMVar_, readMVar)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy.Encoding as T
+import qualified Data.Text.Lazy as T
 import qualified Data.ByteString.Lazy.UTF8 as B8 (toString)
+import qualified Data.ByteString.Lazy (ByteString)
 
-import Network.WebSockets as WS
+import qualified Network.WebSockets as WS
 import Network.Wai (Application)
-import Network.Wai.Handler.Warp as Warp
-import Network.Wai.Handler.WebSockets as WaiWS
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai.Handler.WebSockets as WaiWS
 import qualified Network.Wai.Application.Static as WaiStatic
 
 type Id = Int
@@ -30,27 +33,31 @@ data SS a = SS
 
 initSS a = SS { count = 0, clients = [], world = a }
 
-type Handler a = Text -> a -> (Text, a)
+type Handler a = Data -> a -> IO (Maybe Data, a)
+type Data = ByteString
 
-broadcast :: Id -> Text -> [Client] -> IO ()
+broadcast :: Id -> Data -> [Client] -> IO ()
 broadcast id msg clients = forM_ clients send 
   where
-    send (id', conn) | id /= id' = WS.sendTextData conn msg
+    --send (id', conn) | id /= id' = WS.sendTextData conn msg
+    send (id', conn) = WS.sendTextData conn msg
     send _ = return ()
 
-sendText :: Id -> Text -> MVar (SS a) -> IO ()
-sendText id text state = do
+sendData :: Id -> Data -> MVar (SS a) -> IO ()
+sendData id text state = do
   SS {clients = cs} <- readMVar state
   let Just conn = lookup id cs
+  -- TODO consider sendBinaryData
   WS.sendTextData conn text
 
+talk :: Id -> WS.Connection -> MVar (SS t) -> Handler t -> IO ()
 talk id conn state handler = forever $ do
   msg <- WS.receiveData conn
-  T.putStrLn $ "got: " `mappend` msg
+  T.putStrLn $ "got: " `mappend` (T.toStrict $ T.decodeUtf8 $ msg) -- TODO remove
   SS {clients = cs, world = a} <- readMVar state
-  let (m, a') = handler msg a
+  (m, a') <- handler msg a
   modifyMVar_ state $ \s -> return $ s {world = a'}
-  broadcast id m cs
+  maybe (return ()) (\m -> broadcast id m cs) m
 
 application :: MVar (SS a) -> Handler a -> WS.PendingConnection -> IO ()
 application state handler pending = do
@@ -58,13 +65,13 @@ application state handler pending = do
   WS.forkPingThread conn 30
   SS {world = w, count = count} <- readMVar state
   let id = count + 1
-  putStrLn $ "User #" ++ show id ++ " connected?"
+  putStrLn $ "User #" ++ show id ++ " connected"
   modifyMVar_ state $ \s -> return s {count = id, clients = (id, conn) : clients s}
 
   -- TODO send initial msg --
 
   --send id (T.pack . B8.toString . encode . t2v . n2t w $ root w) state
-  sendText id "hello down there!" state
+  sendData id "hello down there!" state
 
   -- Main loop
   finally (talk id conn state handler) (disconnect id state)
@@ -73,7 +80,7 @@ staticContent :: Network.Wai.Application
 staticContent = WaiStatic.staticApp $ WaiStatic.defaultWebAppSettings "./static"
 
 disconnect id state = do
-  putStrLn $ "User #" ++ show id ++ " disconnected?"
+  putStrLn $ "User #" ++ show id ++ " disconnected"
   modifyMVar_ state $ \s -> return $ removeClient id s
   --SS {clients = cs} <- readMVar state
   --broadcast id (prefixMsg id "disconnected.") cs
@@ -83,4 +90,6 @@ removeClient id s = s { clients = filter (not . (== id) . fst) (clients s) }
 -- Entry point --
 runServer a fn = do
   state <- newMVar (initSS a)
-  Warp.run 8080 (WaiWS.websocketsOr WS.defaultConnectionOptions (application state fn) staticContent)
+  Warp.run 8080 (WaiWS.websocketsOr WS.defaultConnectionOptions
+                                    (application state fn)
+                                    staticContent)
